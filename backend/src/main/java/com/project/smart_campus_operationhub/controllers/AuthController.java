788ff19f63309f65bf2a85dc7062aad91d155033
Pathum Bandara <pathumbandara1234@gmail.com>
2026-04-10 21:49:48 +0530

    Login with email and password

---
+++ backend/src/main/java/com/project/smart_campus_operationhub/controllers/AuthController.java
@@ -0,0 +1,37 @@
+package com.project.smart_campus_operationhub.controllers;
+
+import com.project.smart_campus_operationhub.dtos.LoginRequest;
+import com.project.smart_campus_operationhub.repositories.UserRepository;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
+        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
