88205a09810fbde11fb56fc402f4d42a94ad78ce
Pathum Bandara <pathumbandara1234@gmail.com>
2026-04-11 22:52:39 +0530

    Validate web token

--- backend/src/main/java/com/project/smart_campus_operationhub/services/JwtService.java
+++ backend/src/main/java/com/project/smart_campus_operationhub/services/JwtService.java
@@ -1,11 +1,12 @@
 package com.project.smart_campus_operationhub.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
@@ -20,5 +21,28 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            var claims = getClaims(token);
+
+            return claims.getExpiration().after(new Date());
+        }
+        catch (JwtException ex) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
     }
 }
