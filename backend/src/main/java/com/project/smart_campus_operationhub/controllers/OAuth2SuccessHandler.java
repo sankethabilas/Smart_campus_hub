32cf7978f3cc9d88d3a1e30d992ce2dec1bf661d
sankethabilas21 <sankethabilas21@gmail.com>
2026-04-25 13:17:49 +0530

    Revert "Merge branch 'dev' into feature/incident_ticket_ms"
    This reverts commit 45aec93ab338b14adecbc0d9e1ad9661fdf3492b, reversing
    changes made to b4923fe54c59d7f7838474c5c906ba6593398f89.

--- backend/src/main/java/com/project/smart_campus_operationhub/controllers/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/project/smart_campus_operationhub/controllers/OAuth2SuccessHandler.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 @Component
 @AllArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
     private final AuthService authService;
 
     @Override
@@ -26,8 +27,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         System.out.println("Success handler triggered");
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String token = authService.handleGoogleLogin(oAuth2User);
-        String targetUrl = "http://localhost:5173/oauth-success?token=" + token;
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
 //        response.sendRedirect("http://localhost:3000/oauth-success?token=" + token);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"token\": \"" + token + "\"}");
     }
 }
--- backend/src/main/java/com/project/smart_campus_operationhub/filters/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/project/smart_campus_operationhub/filters/JwtAuthenticationFilter.java
@@ -7,14 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.List;
 
 @AllArgsConstructor
 @Component
@@ -24,25 +22,23 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var authHeader = request.getHeader("Authorization");
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        var token = authHeader.replace("Bearer ", "");
+        var token = authHeader.replace("Bearer", "");
 
         if (!jwtService.validateToken(token)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        var role = jwtService.getRoleFromToken(token);
-        System.out.println("Role found:" + role);
-        var authority = new SimpleGrantedAuthority("ROLE_" + role);
         var authentication = new UsernamePasswordAuthenticationToken(
                 jwtService.getUserIdFromToken(token),
                 null,
-                List.of(authority)
+                null
         );
         authentication.setDetails(
                 new WebAuthenticationDetailsSource().buildDetails(request)
