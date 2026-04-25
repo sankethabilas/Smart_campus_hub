ff7eac23ee6ba50d31b7fee818374553712a38cc
DIperera <IT23590762@my.sliit.lk>
2026-04-26 02:54:51 +0530

    Try to fix the errors

--- frontend/src/services/axiosUserConfig.ts
+++ frontend/src/services/axiosUserConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userAxiosInstance = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: 'http://localhost:8085',
   headers: {
     'Content-Type': 'application/json',
   },
